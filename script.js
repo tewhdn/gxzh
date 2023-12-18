c5dc6ce995fcc71d1036819af3e411c44d4c3f93
tewhdn <141412523+tewhdn@users.noreply.github.com>
2023-12-18 19:03:07 +0800

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function getAccount() {
+  
   const invitationCode = document.getElementById('invitationCode').value;
 
-
-  if (invitationCode === '123456') {
   
+  if (invitationCode === '123456') {
+    
+
+    
     const accounts = [
       { username: 'shandiana002@outlook.com', password: 'mussster4' },
       { username: 'c429369903@outlook.com', password: 'herobrine1' },
       { username: 'shandiana003@outlook.com', password: 'herobrine2' }
     ];
 
-   
+    
     const randomIndex = Math.floor(Math.random() * accounts.length);
     const selectedAccount = accounts[randomIndex];
 
-
+    
     alert(`账号：${selectedAccount.username}\n密码：${selectedAccount.password}`);
   } else {
     alert('邀请码错误，请重新输入');
@@ -27,3 +30,7 @@ function redirectToSponsor() {
 function sendFeedback() {
   window.location.href = 'mailto:2932944216@qq.com?subject=反馈主题&body=这里是反馈内容';
 }
+
+function closeNotice() {
+  document.getElementById("notice").style.display = "none";
+      }
